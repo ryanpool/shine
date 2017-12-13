02e5d807efb6062b18bb5a3206120dec5ef569d8
rpool <ryan@ryanpool.net>
2017-12-12 23:24:44 -0600

    Chapter 4: Database is seeded with 350,000 fake customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+350_000.times do |i|
+  first = Faker::Name.first_name
+  last = Faker::Name.last_name
+  user = Faker::Internet.user_name("#{first} #{last} #{i}")
+  email = Faker::Internet.email(user)
+
+  Customer.create!(
+              first_name: first,
+              last_name: last,
+              username: user,
+              email: email
+  )
+  print '.' if i % 1000 == 0
+end
+puts
